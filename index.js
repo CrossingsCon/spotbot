8ff88853989a66fd42bdfe38ee107ea37064c2de
Eli Morris-Heft <eli.morris.heft@gmail.com>
2020-09-12 22:00:04 -0500

    Add birthday functionality

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-require ('dotenv-safe').config();
-const { App } = require('@slack/bolt');
+import dotenv from 'dotenv-safe';
+dotenv.config();
 
-const app = new App({
+import bolt from '@slack/bolt';
+import cron from 'cron';
+
+import { getBirthdaysForThisHour } from './lib/birthday.js';
+
+const app = new bolt.App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
 
 /* Add functionality here */
-async function pingEli() {
+async function pingEli(text) {
   const openResponse = await app.client.conversations.open({
     token: process.env.SLACK_BOT_TOKEN,
     users: process.env.STARTUP_PING_TARGET_UID,
@@ -15,10 +20,8 @@ async function pingEli() {
   const post = await app.client.chat.postMessage({
     token: process.env.SLACK_BOT_TOKEN,
     channel: openResponse.channel.id,
-    text: 'Dai stiho!',
+    text,
   });
-
-  console.log(post);
 }
 
 (async () => {
@@ -26,5 +29,13 @@ async function pingEli() {
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
-  pingEli();
+
+  const EVERY_HOUR_AT_1_MINUTE_PAST = '0 1 * * * *';
+  const birthdayJob = new cron.CronJob(EVERY_HOUR_AT_1_MINUTE_PAST, async function() {
+    const birthdaysThisHour = await getBirthdaysForThisHour(app);
+    birthdaysThisHour.forEach(async u => {
+      pingEli(`Happy birthday to <@${u.id}>!`);
+    });
+  }, null, true);
+  console.log('📅 Birthdays crontab scheduled!');
 })();
