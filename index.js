d0330490c2974ffb1996f8ef87558f34f1bb5148
Eli Morris-Heft <eli.morris.heft@gmail.com>
2020-09-14 08:24:10 -0500

    Add channel highlight functionality

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ dotenv.config();
 import bolt from '@slack/bolt';
 import cron from 'cron';
 
+import Store from './lib/store.js';
 import { startBirthdayCronjob } from './lib/birthday.js';
+import { startChannelHighlightCronjob } from './lib/channel-highlight.js';
+
+const store = new Store();
 
 const app = new bolt.App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
 
-/* Add functionality here */
-async function pingEli(text) {
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ SpotBot is running!');
+
+  startBirthdayCronjob(app);
+  startChannelHighlightCronjob(app, store);
+})();
+
+async function pingEli(app, text) {
   const openResponse = await app.client.conversations.open({
     token: process.env.SLACK_BOT_TOKEN,
     users: process.env.STARTUP_PING_TARGET_UID,
@@ -23,12 +34,3 @@ async function pingEli(text) {
     text,
   });
 }
-
-(async () => {
-  // Start the app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ SpotBot is running!');
-
-  startBirthdayCronjob(app);
-})();
