0f86260b5ba6d9a3edba7043be0c06dc3c3c9fdb
Eli Morris-Heft <eli.morris.heft@gmail.com>
2020-09-15 00:07:59 -0500

    Post to the right channels and tweak channel spotlight formatting

---
+++ config/channels.js
@@ -0,0 +1,10 @@
+const GENERAL = 'C1LQU6N3H';
+const SPOTBOT_TESTING_GROUND = 'G01B20H3KK3';
+const PM_ELI = 'U1LPCUSTV';
+
+const CHANNELS = {
+  birthday: SPOTBOT_TESTING_GROUND,
+  channelSpotlight: SPOTBOT_TESTING_GROUND,
+};
+
+export default CHANNELS;
